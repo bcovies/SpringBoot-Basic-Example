77ef821cc613b730505dd4b35dd70d97eec5b36c
Bruno Souto <bcovies@gmail.com>
2023-05-26 18:11:30 -0300

    add

---
+++ demo/src/main/java/com/example/demo/controllers/RootController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+    @GetMapping
+    public String hello(){
+        return "How to use it and how to make";
+    }
+}
